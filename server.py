fede1bbe3aa160ea605ec96c5c17542c363bccec
Krr2023 <123816096+Krr2023@users.noreply.github.com>
2024-02-11 16:52:08 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+serverimport http.server
+import socketserver
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_type = self.headers['content-type']
+        if content_type == 'application/x-www-form-urlencoded':
+            content_length = int(self.headers['content-length'])
+            post_data = self.rfile.read(content_length)
+            # 解析表單資料
+            print(post_data.decode('utf-8'))
+        else:
+            # 不支援的內容類型
+            pass
+
+if __name__ == '__main__':
+    port = 8000
+    server = socketserver.TCPServer(('', port), MyHandler)
+    print('伺服器已啟動，正在監聽埠 {} ...'.format(port))
+    server.serve_forever()
